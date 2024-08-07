00763cdeda5c98003f241617b2ae6c88a97cf29a
Thomas Maye <thomas.maye.33@gmail.com>
2024-08-07 10:55:51 +0200

    Upgrade my code again

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,19 +16,22 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int swapped = -1;
 
-	for (i = 0; i < (size - 1); i++)
+	while (swapped != 0)
 	{
-		swapped = 0;
-
-		for (j = 0; j < (size - i - 1); j++)
+		for (i = 0; i < (size - 1); i++)
 		{
-			if (array[j] > array[j + 1])
+			swapped = 0;
+
+			for (j = 0; j < (size - i - 1); j++)
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				swapped++;
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					swapped++;
+					print_array(array, size);
+				}
 			}
 		}
 	}
